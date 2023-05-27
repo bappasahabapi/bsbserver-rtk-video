ce8847b0467482efcd0f61cc48abf3eef0bd8195
Your GitHub Username <Your GitHub Email Address>
2023-05-27 22:46:32 +0600

    server

---
+++ db.js
@@ -0,0 +1,14 @@
+const todos = require("./data/todos");
+const todos1 = require("./data/todos1");
+const videos = require("./data/videos");
+const videos2 = require("./data/videos2");
+const transactions = require("./data/transactions");
+
+module.exports = {
+  todos,
+  videos,
+  videos2,
+  transactions,
+  todos1,
+}
+
